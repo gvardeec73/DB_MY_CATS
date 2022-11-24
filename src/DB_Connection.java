3da72f957a4a33b4cc3c9863d4bb8bea6cfa4f39
Valeriy Shunin <sadgvardeec@yandex.ru>
2022-11-24 09:50:12 +0400

    added data insertion into table types

---
+++ src/DB_Connection.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+public class DB_Connection {
+    public static Connection connection;
+    public static Statement statement;
+    public static ResultSet resultSet;
+
+    // --------ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ--------
+    public static void Connect(String DB_URL) throws ClassNotFoundException, SQLException
+    {
+        connection = null;
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection(DB_URL);
+        if (connection != null) {
+            DatabaseMetaData meta = connection.getMetaData();
+            System.out.println("The driver name is " + meta.getDriverName());
+            System.out.println("База Подключена!");
+        }
+
+    }
+
+    // --------Создание таблицы--------
+    public static void CreateDBTable() throws ClassNotFoundException, SQLException
+    {
+        statement = connection.createStatement();
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS types (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	type varchar(100) NOT NULL\n"
+                + ");";
+        statement.execute(sql);
+
+        System.out.println("Таблица создана или уже существует.");
+    }
+
+    // --------Заполнение таблицы--------
+    public static void  insert_type(String type) throws SQLException
+    {
+        String sql = "INSERT INTO types (type) VALUES ('" + type + "');";
+        statement.execute(sql);
+
+        System.out.println("строка заполнена");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
 import java.sql.*;
-
+import java.sql.SQLException;
 public class Main {
-    public static void createNewDatabase(String url) {
-
-
-
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
     public static void createNewTable(String url) {
         // SQLite connection string
@@ -35,10 +20,13 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         String fileName = "My_cats.db";
-        String url = "jdbc:sqlite:D:" + fileName;
-        createNewDatabase(url);
-        createNewTable(url);
+        String DB_URL = "jdbc:sqlite:D:" + fileName;
+        DB_Connection.Connect(DB_URL);
+        DB_Connection.CreateDBTable();
+        DB_Connection.insert_type("Абиссинская кошка");
+        DB_Connection.insert_type("Австралийский мист");
+        DB_Connection.insert_type("Американская жесткошерстная");
     }
 }
